9d4a6911fcafd5721fc2d223cdff682f0f881194
KBone12 <kbonehobby@gmail.com>
2018-11-07 13:41:19 +0900

    Add the bass audio library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::os::raw::c_void;
+use std::ptr;
+
+#[link(name = "bass")]
+extern "C" {
+    fn BASS_Init(device: i32, frequency: u32, flags: u32, window: *mut c_void, class_id: *mut c_void) -> bool;
+    fn BASS_Free() -> bool;
+}
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        BASS_Init(-1, 44100, 0, ptr::null_mut(), ptr::null_mut());
+        BASS_Free();
+    }
 }
