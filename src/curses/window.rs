2d206df8422c1f720245d2d354fa0d968412f3ab
kbone <kbonehobby@gmail.com>
2018-11-17 13:57:34 +0900

    Add Pane

--- src/curses/window.rs
+++ src/curses/window.rs
@@ -1,12 +1,16 @@
+use std::collections::HashMap;
 use std::rc::Rc;
 
 use pancurses::{self, Input};
 
-use curses::file_explorer::FileExplorer;
 use bass::music::{Music, State};
+use curses::file_explorer::FileExplorer;
+use curses::pane::{EffectPane, Pane, PaneType, PlaylistPane};
 
 pub struct Window {
     window: pancurses::Window,
+    panes: HashMap<PaneType, Box<Pane>>,
+    current_pane: PaneType,
     music: Option<Rc<Music>>,
 }
 
@@ -21,8 +25,14 @@ impl Window {
 
         window.nodelay(true);
 
+        let mut panes = HashMap::new();
+        panes.insert(PaneType::Play, Box::new(PlaylistPane::new()) as Box<Pane>);
+        panes.insert(PaneType::Effect, Box::new(EffectPane::new()) as Box<Pane>);
+
         Self {
             window,
+            panes,
+            current_pane: PaneType::Play,
             music: None,
         }
     }
@@ -30,16 +40,21 @@ impl Window {
     pub fn show(&mut self) {
         let mut should_close = false;
         while !should_close {
+            self.window.clear();
+
             let (height, width) = self.window.get_max_yx();
             let texts = vec![
                 "<Space>: Toggle the music playing/stopped",
                 "'F': Choose the music file",
                 "'q': Quit",
             ];
-
             texts.iter().enumerate().for_each(|(i, t)| {
-                self.window.mvaddstr((height - texts.len() as i32) / 2 + i as i32, (width - t.len() as i32) / 2, t);
+                self.window.mvaddstr(height - (texts.len() - i) as i32, (width - t.len() as i32) / 2, t);
             });
+            if let Some(ref pane) = self.panes.get(&self.current_pane) {
+                pane.draw(&self);
+            }
+
             self.window.refresh();
 
             while let Some(input) = self.window.getch() {
@@ -61,11 +76,27 @@ impl Window {
                         if path.exists() {
                             self.set_music(Rc::new(Music::from_file(&path)));
                         }
+                    },
+                    _ => {
+                        if let Some(ref mut pane) = self.panes.get_mut(&self.current_pane) {
+                            pane.input(&self.window, &input);
+                        }
                     }
-                    _ => {}
                 }
             }
+
+            if let Some(ref pane) = self.panes.get(&self.current_pane) {
+                self.current_pane = pane.next();
+            }
         }
+    }
+
+    pub fn get_handle(&self) -> &pancurses::Window {
+        &self.window
+    }
+
+    pub fn get_music(&self) -> Option<Rc<Music>> {
+        self.music.clone()
     }
 
     pub fn set_music(&mut self, music: Rc<Music>) {
